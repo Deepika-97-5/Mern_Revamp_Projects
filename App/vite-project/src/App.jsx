2530c717af636315e55ca06c540182bddcf4f9ff
user name <ddeepikab2@gmail.coml>
2025-07-24 19:05:18 +0530

    updates

--- App/vite-project/src/App.jsx
+++ App/vite-project/src/App.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import HeroSection from "./pages/HeroSection/HeroSection";
+import HeaderSection from "./pages/HeaderSection/HeaderSection";
+import Card from "./pages/Card/Card";
+import Footer from "./pages/Footer/Footer";
 
 
 const App = () => {
   return (
     <>
+      <HeaderSection/>
+      <Card/>
       <HeroSection/>
-    </>
+      <Footer/>
+       
+
+    </>  
       
 
   );
 };
 
 export default App;
+
     
